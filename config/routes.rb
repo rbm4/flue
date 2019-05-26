bd107233ba2838720fdbfccdb5f0ca64a7f5f977
Ubuntu <ubuntu@ip-10-0-0-114.us-east-2.compute.internal>
2019-05-26 09:37:06 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#main'
   match 'event', to: 'estimates#newEvent', via: [:post]
-  match 'event/show', to: 'estimates#newEvent', via: [:get]
+  match 'event/show', to: 'estimates#listEvents', via: [:get]
 end
